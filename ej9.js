f336a7a0a01b4c90628817503376dcc6b51d2bdb
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-08-11 12:17:11 +0200

    Ejercicio 9

---
+++ ej9.js
@@ -0,0 +1,11 @@
+//apartado 1
+let random = Math.random();
+console.log(random);
+
+//apartado 2
+let n_pi = Math.PI;
+n_pi = n_pi*1000;
+let pi_redondeado = Math.floor(n_pi)
+pi_redondeado = pi_redondeado/1000;
+console.log(pi_redondeado);
+
