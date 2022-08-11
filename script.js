96b7e089a919e8bb91fa217dc41c8a9bb893b6d5
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-08-11 10:43:38 +0200

    Ejercicio 5

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log(Math.round(Math.random()))
+
+let moneda = () =>{
+    let numero = Math.round(Math.random());
+    if (number % 2 == 0){
+        return "cara"
+    } else {
+        return "cruz"
+    }
+}
+
+console.log(moneda());
