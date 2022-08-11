4b3ad558c68c06465cd58047ed35e71538af4a76
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-08-11 13:52:33 +0200

    Ejercicio 12

---
+++ ej12.js
@@ -0,0 +1,19 @@
+// Apartado 1
+console.log("Apartado 1")
+let letras = ["a","b","c","d","e"];
+letras.forEach(
+    (item) => {
+        console.log(item)
+    }
+)
+
+// Apartado 2
+console.log("Apartado 2");
+let numero = 5;
+while(numero > 0){
+    console.log(numero);
+    numero = numero-1; 
+    if(numero==0){
+        console.log(numero);
+    }
+}
